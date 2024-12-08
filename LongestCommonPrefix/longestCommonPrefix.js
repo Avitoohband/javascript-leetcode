0e10dfdce6523675fe26e978708baba528912980
Avi Tuchband <Avitoohband@gmail.com>
2024-12-08 18:14:13 +0200

    longest common prefix

--- LongestCommonPrefix/longestCommonPrefix.js
+++ LongestCommonPrefix/longestCommonPrefix.js
@@ -2,7 +2,7 @@ const longestCommonPrefix = (strs) => {
     let commonPrefix = "";
   for (let i = 0; i < strs[0].length; i++) {
     for(let j = 0 ; j < strs.length; j++){   
-        if(i >= strs.length[j] || strs[j][i] != strs[0][i]) return commonPrefix;       
+        if(i >= strs[j].length || strs[j][i] != strs[0][i]) return commonPrefix;       
     }
     commonPrefix += strs[0][i];
   }
